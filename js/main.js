7bf225a0113811bc1cb989ac76469b57746261e5
Cnomesta <36128569+Cnomesta@users.noreply.github.com>
2020-10-13 05:06:43 +0300

    Production
    Going for good push

---
+++ js/main.js
@@ -0,0 +1,107 @@
+function emf(){
+  var element = document.getElementById("emflight1");
+  element.classList.toggle("light1");
+  var element = document.getElementById("emflight2");
+  element.classList.toggle("light1");
+  var element = document.getElementById("emflight3");
+  element.classList.toggle("light1");
+  var element = document.getElementById("emflight4");
+  element.classList.toggle("light1");
+  var element = document.getElementById("emflight5");
+  element.classList.toggle("light1");
+  var element = document.getElementById("emflight6");
+  element.classList.toggle("light1");
+  var element = document.getElementById("emfb");
+  element.classList.toggle("light1");
+}
+
+function orb(){
+  var element = document.getElementById("orblight1");
+  element.classList.toggle("light1");
+  var element = document.getElementById("orblight2");
+  element.classList.toggle("light1");
+  var element = document.getElementById("orblight3");
+  element.classList.toggle("light1");
+  var element = document.getElementById("orblight4");
+  element.classList.toggle("light1");
+  var element = document.getElementById("orblight5");
+  element.classList.toggle("light1");
+  var element = document.getElementById("orblight6");
+  element.classList.toggle("light1");
+  var element = document.getElementById("orbb");
+  element.classList.toggle("light1");
+}
+
+function gw(){
+  var element = document.getElementById("gwlight1");
+  element.classList.toggle("light1");
+  var element = document.getElementById("gwlight2");
+  element.classList.toggle("light1");
+  var element = document.getElementById("gwlight3");
+  element.classList.toggle("light1");
+  var element = document.getElementById("gwlight4");
+  element.classList.toggle("light1");
+  var element = document.getElementById("gwlight5");
+  element.classList.toggle("light1");
+  var element = document.getElementById("gwlight6");
+  element.classList.toggle("light1");
+  var element = document.getElementById("gwb");
+  element.classList.toggle("light1");
+}
+
+function ft(){
+  var element = document.getElementById("ftlight1");
+  element.classList.toggle("light1");
+  var element = document.getElementById("ftlight2");
+  element.classList.toggle("light1");
+  var element = document.getElementById("ftlight3");
+  element.classList.toggle("light1");
+  var element = document.getElementById("ftlight4");
+  element.classList.toggle("light1");
+  var element = document.getElementById("ftlight5");
+  element.classList.toggle("light1");
+  var element = document.getElementById("ftlight6");
+  element.classList.toggle("light1");
+  var element = document.getElementById("ftb");
+  element.classList.toggle("light1");
+}
+
+function sb(){
+  var element = document.getElementById("sblight1");
+  element.classList.toggle("light1");
+  var element = document.getElementById("sblight2");
+  element.classList.toggle("light1");
+  var element = document.getElementById("sblight3");
+  element.classList.toggle("light1");
+  var element = document.getElementById("sblight4");
+  element.classList.toggle("light1");
+  var element = document.getElementById("sblight5");
+  element.classList.toggle("light1");
+  var element = document.getElementById("sblight6");
+  element.classList.toggle("light1");
+  var element = document.getElementById("sblight7");
+  element.classList.toggle("light1");
+  var element = document.getElementById("sbb");
+  element.classList.toggle("light1");
+}
+
+function fp(){
+  var element = document.getElementById("fplight1");
+  element.classList.toggle("light1");
+  var element = document.getElementById("fplight2");
+  element.classList.toggle("light1");
+  var element = document.getElementById("fplight3");
+  element.classList.toggle("light1");
+  var element = document.getElementById("fplight4");
+  element.classList.toggle("light1");
+  var element = document.getElementById("fplight5");
+  element.classList.toggle("light1");
+  var element = document.getElementById("fpb");
+  element.classList.toggle("light1");
+}
+/*
+function clear(){
+  var element = document.getElementById("emflight1")
+  element.classList.toggle("lightoff")
+}
+*/
